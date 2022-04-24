953a02ddf9f55ba012307b3b0f705b8f55ceea04
Вячеслав Синников <sinnikovvyacheslav@mail.ru>
2022-04-24 12:23:14 +0500

    add files

---
+++ src/com/company/Person.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+
+    public Person(String name, String surname) {
+        this(name, surname, 0);
+
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age != 0;
+    }
+
+    public boolean hasAddres() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        }
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(surname)
+                .setAge(5)
+                .setAddress(address);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + " возраст " + age +
+                ", город " + address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
+}
---
+++ src/com/company/PersonBuilder.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class PersonBuilder {
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0 || age > 100) {
+            throw new IllegalArgumentException("Возраст недопустимый!");
+        }
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Не хватает обяхательных полей!");
+        }
+        Person person = new Person(name, surname, age);
+        person.setAddress(address);
+
+        return person;
+    }
+}
